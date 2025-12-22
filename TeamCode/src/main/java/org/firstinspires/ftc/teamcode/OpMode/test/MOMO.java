e9124b8b399087bcb3b3664b82c670f42bbda421
hkskyrocket <155718728+hkskyrocket@users.noreply.github.com>
2025-12-22 11:09:36 +0800

    Update MOMO.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/test/MOMO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/test/MOMO.java
@@ -1,21 +1,18 @@
 package org.firstinspires.ftc.teamcode.OpMode.test;
-
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-
-@TeleOp(name = "MOMO",group = "TeleOP")
+@Autonomous(name = "MOMO",group = "TeleOP")
 public class MOMO extends LinearOpMode {
 
     private DcMotor leftUp, rightUp, leftDown, rightDown;
+    private ElapsedTime timer = new ElapsedTime();
+    private double x, y, r;
+    private boolean playerDrive = true;
 
     @Override
     public void runOpMode() {
@@ -24,40 +21,33 @@ public class MOMO extends LinearOpMode {
         leftDown = hardwareMap.get(DcMotor.class, "leftDown");
         rightDown = hardwareMap.get(DcMotor.class, "rightDown");
 
-        leftUp.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightUp.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDown.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDown.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftUp.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightUp.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftDown.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightDown.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        telemetry.addData("Status", "Initialized");
-        telemetry.update(); // Display the "Initialized" message
-        rightDown.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightUp.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftDown.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftUp.setDirection(DcMotorSimple.Direction.FORWARD);
 
         waitForStart();
 
-        while (opModeIsActive()) {
+        if (isStopRequested()) {
+            return;
+        }
+        moveForwardTime(2);
+        StrafeTime(2);
+    }
 
-            double x = gamepad1.left_stick_x;
-            double y = gamepad1.left_stick_y;
-            double r = gamepad1.right_stick_x;
+    public void moveForwardTime(double time) {
+        timer.reset();
+        while (timer.time() < time) {
+            leftUp.setPower(1);
+            rightUp.setPower(1);
+            leftDown.setPower(1);
+            rightDown.setPower(1);
+        }
+    }
 
-            telemetry.addData("Encoder Value", leftUp.getCurrentPosition());
-            telemetry.addData("Encoder Value", rightUp.getCurrentPosition());
-            telemetry.addData("Encoder Value", leftDown.getCurrentPosition());
-            telemetry.addData("Encoder Value", rightDown.getCurrentPosition());
-
-            leftUp.setPower(0.4 * (x + y - r));
-            rightUp.setPower(0.4 * (x - y - r));
-            leftDown.setPower(0.4 * (-x + y - r));
-            rightDown.setPower(0.4 * (-x - y - r));
+    public void StrafeTime(double time) {
+        timer.reset();
+        while (timer.time() < time) {
+            leftUp.setPower(1);
+            rightUp.setPower(-1);
+            leftDown.setPower(1);
+            rightDown.setPower(-1);
         }
     }
 }
-
-//Our Program
