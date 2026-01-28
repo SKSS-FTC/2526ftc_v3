ef4d259fe02afb080b730a8e61b0c1e0e46d6c11
Ryan0166 <ryanfong0116@gmail.com>
2026-01-29 00:46:26 +0800

    Update AutoPathing.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoPathing.java
@@ -1,64 +1,14 @@
-package org.firstinspires.ftc.teamcode.pedroPathing.;
-
+package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.pedropathing.util.Timer;
-
-@Autonomous
-public class AutoPathing extends OpMode {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+@Autonomous(name = "Example Auto", group = "Examples")
+public class ExampleAuto extends OpMode {
     private Follower follower;
-    private Timer pathTimer, opModeTimer;
-
-    public enum PathState {
-        DRIVE_STARTPOS_SHOOT_POS, SHOOT_PRELOAD
-        SHOOT_PRELOAD
-
-    }
-    PathState pathState;
-
-    private final Pose startpose = new Pose(72,20,90);
-    private final Pose shootpose = new Pose(72,120,90);
-
-    private PathChain driverStartPosShootPos;
-
-    public void buildPaths() {
-        driverStartPosShootPos = follower.pathBuilder()
-                .addPath(new BezierLine(startpose, shootpose))
-                .setLinearHeadingInterpolation(startpose.getHeading(), shootpose.getHeading())
-                .build();
-    }
-
-    public void StatePathUpdate() {
-        switch (pathState) {
-            case DRIVE_STARTPOS_SHOOT_POS:
-                follower.followPath(driverStartPosShootPos, true);
-                pathState = PathState.SHOOT_PRELOAD;
-                break;
-            case SHOOT_PRELOAD:
-                if (!follower.isBusy()) {
-                    telemetry.addLine("Done Path 1 ");
-
-                }
-                break;
-            default:
-                telemetry.addLine("No State Commanded");
-                break;
-        }
-    }
-
-    public void set
-
-
-    @Override
-    public void init() {
-
-    }
-    @Override
-    public void loop() {
-
-    }
-}
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private int pathState;
